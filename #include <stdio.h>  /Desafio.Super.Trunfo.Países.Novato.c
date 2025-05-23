f6db9ab06d7354f2692b553f69cbf2babbf48c1a
AngelFabioLima <angelfabiolima@gmail.com>
2025-05-22 21:11:25 -0400

    Update Desafio.Super.Trunfo.Países.Novato.c

--- #include <stdio.h>  /Desafio.Super.Trunfo.Países.Novato.c
+++ #include <stdio.h>  /Desafio.Super.Trunfo.Países.Novato.c
@@ -1,3 +1,20 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
+int main() {
+    // Declaração das variáveis da Carta 1
+    char estado1;
+    char codigo1[4];
+    char nomeCidade1[100];
+    int populacao1;
+    float area1;
+    float pib1;
+    int pontosTuristicos1;
+
+    // Declaração das variáveis da Carta 2
+    char estado2;
+    char codigo2[4];
+    char nomeCidade2[100];
+    int populacao2;
+    float area2;
+    float pib2;
+    int pontosTuristicos2;

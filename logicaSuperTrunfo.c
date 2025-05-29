69787ca349b4a0bdc1c76961dfe72cc2ed3dedf0
AngelFabioLima <angelfabiolima@gmail.com>
2025-05-29 01:55:48 +0000

    melhoras 01 add as cartas

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -3,7 +3,6 @@
 // Desafio Super Trunfo - Países
 // Tema 2 - Comparação das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de comparação de cartas de cidades. 
-// Siga os comentários para implementar cada parte do desafio.
 
 int main() {
     // Definição das variáveis para armazenar as propriedades das cidades
@@ -21,25 +20,62 @@ typedef struct {
     float pib_per_capita;
 } Carta;
 
-        
-    // Cadastro das Cartas:
-    Carta carta1, carta2;
+int menuAtributos(char *mensagem) { // os atributos das cartas
+    int opcao;
+    printf("\n%s\n", mensagem);
+    printf("1. População\n");
+    printf("2. Área\n");
+    printf("3. PIB\n");
+    printf("4. Pontos Turísticos\n");
+    printf("5. Densidade Demográfica\n");
+    printf("Escolha (1-5): ");
+    scanf("%d", &opcao);
+    return opcao;
+}
 
-strcpy(carta1.estado, "Roraima");
-...
-carta1.populacao = 400000;
-...
+// Função que retorna o valor de um atributo específico
+float valorAtributo(Carta c, int atributo) {
+    switch (atributo) {
+        case 1: return (float)c.populacao;
+        case 2: return c.area;
+        case 3: return c.pib;
+        case 4: return (float)c.pontos_turisticos;
+        case 5: return c.densidade;
+        default: return 0.0;
+    }
+}    
+// Função que compara dois valores considerando regra da densidade
+int compararAtributo(float val1, float val2, int atributo) {
+    if (atributo == 5) {
+        // Menor valor vence para densidade
+        return (val1 < val2) ? 1 : (val2 < val1 ? 2 : 0);
+    } else {
+        // Maior valor vence para os demais
+        return (val1 > val2) ? 1 : (val2 > val1 ? 2 : 0);
+    }
+}
+   int main() {
+    // Criando as cartas manualmente
+    Carta c1 = {"Amazonas", "AM01", "Manaus", 2200000, 11400.0, 52.0, 12, 0};
+    Carta c2 = {"Roraima", "RR01", "Boa Vista", 400000, 5700.0, 7.5, 5, 0};
 
-    // if (populacaoA > populacaoB) {
-    //     printf("Cidade 1 tem maior população.\n");
-    // } else {
-    //     printf("Cidade 2 tem maior população.\n");
-    // }
+    // Calculando densidade
+    c1.densidade = c1.populacao / c1.area;
+    c2.densidade = c2.populacao / c2.area;
 
-    // Exibição dos Resultados:
-    // Após realizar as comparações, exiba os resultados para o usuário.
-    // Certifique-se de que o sistema mostre claramente qual carta venceu e com base em qual atributo.
+    int attr1 = menuAtributos("Escolha o primeiro atributo para comparar:");
 
+    // Cálculo automático da densidade e PIB per capita
+    c1.densidade = c1.populacao / c1.area;
+    c2.densidade = c2.populacao / c2.area;
+    c1.pib_per_capita = (c1.pib * 1000000000) / c1.populacao;
+    c2.pib_per_capita = (c2.pib * 1000000000) / c2.populacao;
+
+    // Exibe informações básicas das cartas
+    printf("=== Super Trunfo - Cidades ===\n");
+    printf("Carta 1: %s (%s)\n", c1.cidade, c1.estado);
+    printf("Carta 2: %s (%s)\n", c2.cidade, c2.estado);
+    
     // Exemplo:
     // printf("A cidade vencedora é: %s\n", cidadeVencedora);
 

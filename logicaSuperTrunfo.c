110e8b6074cba0ebd7b5ccf64e580873cdfa683e
AngelFabioLima <angelfabiolima@gmail.com>
2025-05-28 21:31:35 -0400

    Update logicaSuperTrunfo.c

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -8,11 +8,78 @@
 int main() {
     // Definição das variáveis para armazenar as propriedades das cidades
     // Você pode utilizar o código do primeiro desafio
-
-    
+ Estrutura da carta
+typedef struct {
+    char nome[30];
+    int populacao;
+    float area;
+    float pib;
+    int pontos_turisticos;
+    float densidade;
+} Carta;
+        
     // Cadastro das Cartas:
+     char estado1 = 'A';
+    char codigo1[4] = "A1";
+    char nomeCidade1[100] = "Manaus";
+    int populacao1 = 12345689;
+    float area1 = 248.83;
+    float pib1 = 13094.94;
+    int pontosTuristicos1 = 34;
+
+    char estado2 = 'B';
+    char codigo2[4] = "B1";
+    char nomeCidade2[100] = "Bahia";
+    int populacao2 = 32145698;
+    float area2 = 456.98;
+    float pib2 = 65000.00;
+    int pontosTuristicos2 = 102;
     // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
+        printf("Cadastro da Carta 1:\n");
+
+    printf("Estado (A-H): ");
+    scanf(" %c", &estado1);
+
+    printf("Código da Carta: ");
+    scanf("%s", codigo1);
+
+    printf("Nome da Cidade: ");
+    scanf(" %[^\n]", nomeCidade1);
+
+    printf("População: ");
+    scanf("%d", &populacao1);
+
+    printf("Área (em km²): ");
+    scanf("%f", &area1);
+
+    printf("PIB (em bilhões de reais): ");
+    scanf("%f", &pib1);
+
+    printf("Número de Pontos Turísticos: ");
+    scanf("%d", &pontosTuristicos1);
     // utilizando a função scanf para capturar as entradas.
+     printf("\nCadastro da Carta 2:\n");
+
+    printf("Estado (A-H): ");
+    scanf(" %c", &estado2);
+
+    printf("Código da Carta: ");
+    scanf("%s", codigo2);
+
+    printf("Nome da Cidade: ");
+    scanf(" %[^\n]", nomeCidade2);
+
+    printf("População: ");
+    scanf("%d", &populacao2);
+
+    printf("Área (em km²): ");
+    scanf("%f", &area2);
+
+    printf("PIB (em bilhões de reais): ");
+    scanf("%f", &pib2);
+
+    printf("Número de Pontos Turísticos: ");
+    scanf("%d", &pontosTuristicos2);
     // utilize o código do primeiro desafio
 
     // Exemplo:
